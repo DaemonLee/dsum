50476f04d645db57e9e1634f6534c3ec51fd9517
DaemonLee <DaemonLeeSchmidt@gmail.com>
2014-05-20 02:29:26 -0700

    Fixed license and added more info

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 dsum
 ====
 
-dsum is an early prototype for a universal hash integrity checker with an automatic file name parser for commonly checked things.  
+dsum is an early prototype for a universal hash integrity checker with an automatic file name parser for commonly checked things. 
+
+INFO
+====
+
+Classic make ; make install. make uninstall to remove.
+
+Usage is simple, dsum <Optional hashing method, defaults to MD5> filename
+
+Licensed under GPLv3 so feel free to fork. (Maybe you can make it not garbage. *wink*)
+
+TODO
+====
+Make it not case sensitive.
+Add the file name parser, currently planned: Basic .iso and the like defaulting to MD5 and videos with CRC data defauling to CRC32 with an auto "Everything checks out" alert.
+Add more hashing methods.
--- dsumCalculateHashes.cpp
+++ dsumCalculateHashes.cpp
@@ -1,3 +1,19 @@
+/*	dsum - A universal hash integrity checker with an automatic file name parser for commonly checked things.
+	Copyright (C) 2014  Daemon Lee Schmidt
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include "dsumCalculateHashes.h"
 
 std::string calculateMD5ToString(char *filename)
@@ -37,9 +53,9 @@ unsigned long calculateCRC32(char *filename)
 
 	while ((bytes = fread (buffer, 1, length, inFile)) != 0) {
 		crc = crc32(crc, buffer, length);
-    }
-    
-    fclose (inFile);
+	}
+	
+	fclose (inFile);
 
-    return crc;
+	return crc;
 }
