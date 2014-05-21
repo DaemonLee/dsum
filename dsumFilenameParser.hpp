eac260b72855b2f947d81005ddaa1e57a9e74a54
DaemonLee <DaemonLeeSchmidt@gmail.com>
2014-05-21 00:52:35 -0700

    Added the first steps of the parser plus cleaned some stuff up.

---
+++ dsumFilenameParser.hpp
@@ -0,0 +1,26 @@
+/*	dsum - A universal hash integrity checker with an automatic file name parser for commonly checked things.
+	Copyright 2014 Daemon Lee Schmidt
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+    	http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+#ifndef DSUMFILENAME_HPP
+#define DSUMFILENAME_HPP
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+#endif
+
+bool checkExtension (std::string const &filename, std::string const &extension);
